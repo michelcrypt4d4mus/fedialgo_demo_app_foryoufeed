fef696d4fa9e6b727309c51c78b0b9f662d1926d
ashariyar <ashariyar@users.noreply.github.com>
2025-05-16 01:59:38 -0400

    Build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-NODE_ENV=production npx webpack --mode production
+FEDIALGO_DEBUG=false QUICK_MODE=false NODE_ENV=production npx webpack --mode production
 git commit -am"Build"
 git push origin github_pages
