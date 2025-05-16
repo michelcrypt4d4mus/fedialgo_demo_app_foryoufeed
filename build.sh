560b1ea99f036786b9fcf78893e83b2e19d779fe
ashariyar <ashariyar@users.noreply.github.com>
2025-05-16 02:37:13 -0400

    fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-BUILD_GITHUB_PAGE=true FEDIALGO_DEBUG=false QUICK_MODE=false NODE_ENV=production npx webpack --mode production
+BUILD_GITHUB_PAGES=true FEDIALGO_DEBUG=false QUICK_MODE=false NODE_ENV=production npx webpack --mode production
 git commit -am"Build"
 git push origin github_pages
