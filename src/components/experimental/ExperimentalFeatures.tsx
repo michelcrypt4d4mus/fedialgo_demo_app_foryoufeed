949a07a747f7b1fcb893adb34f0a590f53ca2bd2
ashariyar <ashariyar@users.noreply.github.com>
2025-05-29 11:18:00 -0400

    showstats button text

--- src/components/experimental/ExperimentalFeatures.tsx
+++ src/components/experimental/ExperimentalFeatures.tsx
@@ -28,7 +28,7 @@ const logger = new ComponentLogger("ExperimentalFeatures");
 const BUTTON_TEXT = {
     [DELETE_ALL]: "Wipe all user data including the registered app. Necessary to handle OAuth permissions errors." +
                   " You'll need to reauthenticate afterwards.",
-    [SCORE_STATS]: "Show some confusing charts breaking down the way your configuration is scoring your timeline broken into deciles.",
+    [SCORE_STATS]: "Show some charts breaking down the way your timeline is being scored when looked at in deciles.",
     [SHOW_STATE]: `Show a bunch of information about ${FEDIALGO}'s internal state.`,
     [LOAD_COMPLETE_USER_HISTORY]: "Load all your toots and favourites. May improve scoring of your feed." +
                                   " Takes time & resources proportional to the number of times you've tooted.",
