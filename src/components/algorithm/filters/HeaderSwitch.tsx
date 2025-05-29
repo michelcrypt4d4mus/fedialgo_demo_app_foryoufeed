9c76772ab64f77e68f5865add32b56b38ee366c8
ashariyar <ashariyar@users.noreply.github.com>
2025-05-29 11:10:40 -0400

    move SwitchType to config.ts; configure filter header switch tooltips in main config

--- src/components/algorithm/filters/HeaderSwitch.tsx
+++ src/components/algorithm/filters/HeaderSwitch.tsx
@@ -7,20 +7,8 @@ import { ChangeEvent } from "react";
 import { Tooltip } from 'react-tooltip';
 
 import FilterCheckbox from "./FilterCheckbox";
-import { config } from "../../../config";
+import { config, SwitchType } from "../../../config";
 import { TOOLTIP_ANCHOR, tooltipZIndex } from "../../../helpers/style_helpers";
-
-export enum SwitchType {
-    HIGHLIGHTS_ONLY = "highlightsOnly",
-    INVERT_SELECTION = "invertSelection",
-    SORT_BY_COUNT = "sortByCount",
-};
-
-const SWITCH_TOOLTIPS: Record<SwitchType, string> = {
-    [SwitchType.HIGHLIGHTS_ONLY]: "Only show the colored options",
-    [SwitchType.INVERT_SELECTION]: "Exclude the selected options",
-    [SwitchType.SORT_BY_COUNT]: "Sort the options by the number of toots instead of alphabetically",
-};
 
 const HEADER_SWITCH_TOOLTIP_ANCHOR = `${TOOLTIP_ANCHOR}-header-switch`;
 
@@ -53,7 +41,7 @@ export default function HeaderSwitch(props: HeaderSwitchProps) {
             onChange={onChange}
             tooltip={{
                 anchor: HEADER_SWITCH_TOOLTIP_ANCHOR,
-                text: tooltipText || SWITCH_TOOLTIPS[label],
+                text: tooltipText || config.tooltips.filterSwitchTooltips[label],
             }}
         />
     );
