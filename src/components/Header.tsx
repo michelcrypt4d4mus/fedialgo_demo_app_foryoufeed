8ad36314afbf40c716b58958167637abac141905
ashariyar <ashariyar@users.noreply.github.com>
2025-06-11 01:26:50 -0400

    Larger font for version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
             <Row className='w-100 m-1'>
                 <Col xs={XS_VALUE} className="p-0">
                     {user &&
-                        <div className='text-center d-inline align-middle'>
+                        <div className='align-middle d-inline text-center'>
                             {user?.profilePicture &&
                                 <img
                                     alt="Avatar"
@@ -35,17 +35,17 @@ export default function Header() {
 
                 <Col xs={XS_VALUE} className='text-center p-0'>
                     <img
-                        className="d-inline-block align-middle"
+                        className="align-middle d-inline-block"
                         src={config.app.headerIconUrl}
                         style={avatarStyle}
                     />
 
-                    <span className='text-center align-middle p-2' style={fedialgoContainer}>
+                    <span className='align-middle p-2 text-center' style={fedialgoContainer}>
                         <a href={config.app.repoUrl} style={{color: "white"}} target="_blank">
                             Fedialgo Demo
                         </a>
 
-                        {' '}<span style={{color: "lightgrey", fontSize: 10}}>(
+                        {' '}<span style={versionStyle}>(
                             <a href={config.app.changelogUrl} style={{color: "grey"}} target="_blank">
                                 v{process.env.FEDIALGO_VERSION}
                             </a>
@@ -55,7 +55,12 @@ export default function Header() {
 
                 <Col xs={XS_VALUE} className='text-end p-0'>
                     {user &&
-                        <Button className='p-2 text-center' onClick={() => logout()} size="sm" variant="outline-danger">
+                        <Button
+                            className='p-2 text-center'
+                            onClick={() => logout()}
+                            size="sm"
+                            variant="outline-danger"
+                        >
                             Logout
                         </Button>}
                 </Col>
@@ -74,4 +79,9 @@ const avatarStyle: CSSProperties = {
 const fedialgoContainer: CSSProperties = {
     fontSize: 16,
     whiteSpace: "nowrap",
+};
+
+const versionStyle: CSSProperties = {
+    fontSize: 13,
+    color: "lightgrey",
 };
