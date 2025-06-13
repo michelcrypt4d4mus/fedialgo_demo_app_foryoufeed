1ca57bfdcb110befdba9b3b1ebb67c21f9fd2510
ashariyar <ashariyar@users.noreply.github.com>
2025-06-13 10:26:01 -0400

    comment

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -53,7 +53,7 @@ export default function AuthProvider(props: PropsWithChildren) {
         body.append("client_secret", app.clientSecret);
         const oauthRevokeURL = user.server + '/oauth/revoke';
 
-        // POST to oauthRevokeURL throws error but log show "Status code: 200" so I think it works? Hard to
+        // POST to oauthRevokeURL throws error but log shows "Status code: 200" so I think it works? Hard to
         // get at the actual status code variable (it's only in the low level logs).
         // Error: "Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at https://universeodon.com/oauth/revoke. (Reason: CORS header ‘Access-Control-Allow-Origin’ missing). Status code: 200.""
         try {
